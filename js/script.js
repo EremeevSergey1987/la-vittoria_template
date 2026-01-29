92f9f7780ff82028cb29d3a4e2e650290a93f30b
Sovetskaya_Office <eremeev87@bk.ru>
2026-01-29 11:45:17 +0800

    12312

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
         var res = $(".block_cat");
         $('[rel^="m"]').on("click", funk);
+
         $(document).click(function(e) {
             if ($(e.target).closest(res).length || $(e.target).closest('.knop').length) return;
             res.fadeOut(100);
         });
+
         function funk(){
             var link = $(this).attr('rel'),
                 el = $('.block_cat.'+link);
